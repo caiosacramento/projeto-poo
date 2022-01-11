b5a585a5e4d187c1dba07f57dd866e6cd620cb73
Caio sacramento <caiosacramento@outlook.com>
2022-01-11 17:00:16 -0300

    tratamento de erro no metodo de atualizar status

--- todolist.py
+++ todolist.py
@@ -6,24 +6,28 @@ class Tarefa:
     self.categoria = categoria
     self.status = status
     self.ativo = ativo
-  
+
   def alterar_status(titulo):
+    contador = 0
     with open ('base_de_dados.csv') as arquivo: 
       tabela = csv.reader(arquivo, delimiter=';', lineterminator='\n') 
  
       conteudo = list(tabela) 
- 
+  
     for linha in conteudo: 
-      if linha[0] == titulo:  
+      if linha[0] == titulo:
+        contador += 1  
         if linha[3] == 'Pendente':
           linha[3] = 'Concluido'
         elif linha[3] == 'Concluido':
           linha[3] = 'Pendente'
-
-    with open ('base_de_dados.csv', 'w') as arquivo: 
-      escritor = csv.writer(arquivo, delimiter=';', lineterminator='\n') 
-      escritor.writerows(conteudo)
-    print(f'o status da atividade de titulo {titulo} foi atualizado!')
+    if contador == 0:
+      print("titulo inexistente")
+    else:    
+      with open ('base_de_dados.csv', 'w') as arquivo: 
+        escritor = csv.writer(arquivo, delimiter=';', lineterminator='\n') 
+        escritor.writerows(conteudo)
+      print(f'o status da atividade de titulo {titulo} foi atualizado!')
 
   def remover_tarefa(titulo):
     with open ('base_de_dados.csv') as arquivo: 
@@ -67,6 +71,7 @@ while menu!='0':
     categoria = input('Digite a categoria da tarefa: ')
 
     tarefa = Tarefa(titulo, data, categoria)
+    
     tarefa.criar_tarefa()
   
   if menu == '2':
